3efca14499995b85508126267162d6153a0f9af2
Victor Moraes <vrme@gft.com>
2023-07-22 15:32:00 -0300

    Refatorando requests e removendo o stub que não parece mais ser necessário

--- cypress/e2e/barriga/frontend.cy.js
+++ cypress/e2e/barriga/frontend.cy.js
@@ -11,9 +11,10 @@ import '../../support/commandsContas';
 import buildEnv from '../../support/buildEnv';
 
 describe('Should test at a functional level', () => {
-  after(() => {
-    cy.clearLocalStorage();
-  });
+  // No longer needed. Latest versions of cypress already clear local storage automatically
+  // after(() => {
+  //   cy.clearLocalStorage();
+  // });
 
   beforeEach(() => {
     buildEnv();
@@ -23,81 +24,70 @@ describe('Should test at a functional level', () => {
   });
 
   it('Should test responsiveness', () => {
-    cy.get(loc.MENU.HOME).should('exist')
-      .and('be.visible');
+    cy.get(loc.MENU.HOME).should('exist').and('be.visible');
 
     cy.viewport(500, 700);
-    cy.get(loc.MENU.HOME).should('exist')
-      .and('not.be.visible');
+    cy.get(loc.MENU.HOME).should('exist').and('not.be.visible');
     cy.viewport('samsung-s10');
-    cy.get(loc.MENU.HOME).should('exist')
-      .and('not.be.visible');
+    cy.get(loc.MENU.HOME).should('exist').and('not.be.visible');
     cy.viewport('ipad-2');
-    cy.get(loc.MENU.HOME).should('exist')
-      .and('be.visible');
+    cy.get(loc.MENU.HOME).should('exist').and('be.visible');
   });
 
   it('Should create an account', () => {
-    cy.route({
-      method: 'POST',
-      url: '/contas',
-      response: [
-        {
-          id: 3, nome: 'Conta de teste', visivel: true, usuario_id: 1000,
-        },
-      ],
+    cy.intercept('POST', '/contas', {
+      id: 3,
+      nome: 'Conta de teste',
+      visivel: true,
+      usuario_id: 1000,
     }).as('saveConta');
 
     cy.acessarMenuConta();
 
-    cy.route({
-      method: 'GET',
-      url: '/contas',
-      response: [
-        {
-          id: 1, nome: 'Carteira', visivel: true, usuario_id: 1000,
-        },
-        {
-          id: 2, nome: 'Banco', visivel: true, usuario_id: 1000,
-        },
-        {
-          id: 3, nome: 'Conta de teste', visivel: true, usuario_id: 1000,
-        },
-      ],
-    }).as('contasSave');
+    cy.intercept('GET', '/contas', [
+      {
+        id: 1,
+        nome: 'Carteira',
+        visivel: true,
+        usuario_id: 1000,
+      },
+      {
+        id: 2,
+        nome: 'Banco',
+        visivel: true,
+        usuario_id: 1000,
+      },
+      {
+        id: 3,
+        nome: 'Conta de teste',
+        visivel: true,
+        usuario_id: 1000,
+      },
+    ]).as('contasSave');
 
     cy.inserirConta('Conta de teste');
     cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso');
   });
 
   it('Should update an account', () => {
-    cy.route({
-      method: 'PUT',
-      url: '/contas/**',
-      response: [
-        {
-          id: 1, nome: 'Conta alterada', visivel: true, usuario_id: 1000,
-        },
-      ],
+    cy.intercept('PUT', '/contas/**', {
+      id: 1,
+      nome: 'Conta alterada',
+      visivel: true,
+      usuario_id: 1000,
     }).as('contasUpdate');
 
     cy.acessarMenuConta();
     cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Carteira')).click();
-    cy.get(loc.CONTAS.NOME)
-      .clear()
-      .type('Conta alterada');
+    cy.get(loc.CONTAS.NOME).clear().type('Conta alterada');
     cy.get(loc.CONTAS.BTN_SALVAR).click();
     cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso');
   });
 
   it('Should not create an account with same name', () => {
-    cy.route({
-      method: 'POST',
-      url: '/contas',
-      response: [
-        { error: 'Já existe uma conta com esse nome!' },
-      ],
-      status: 400,
+    cy.intercept('POST', '/contas', {
+      body: [{ error: 'Já existe uma conta com esse nome!' }],
+      statusCode: 400,
     }).as('saveContaMesmoNome');
 
     cy.acessarMenuConta();
@@ -108,31 +98,25 @@ describe('Should test at a functional level', () => {
   });
 
   it('Shoud create a transaction', () => {
-    cy.route({
-      method: 'POST',
-      url: 'transacoes',
-      response: [{
-        conta_id: 245134,
-        data_pagamento: '2020-08-22T03:00:00.000Z',
-        data_transacao: '2020-08-22T03:00:00.000Z',
-        descricao: 'Desc',
-        envolvido: 'Inter',
-        id: 219754,
-        observacao: null,
-        parcelamento_id: null,
-        status: true,
-        tipo: 'REC',
-        transferencia_id: null,
-        usuario_id: 11053,
-        valor: '123.00',
-      }],
+    cy.intercept('POST', 'transacoes', {
+      conta_id: 245134,
+      data_pagamento: '2020-08-22T03:00:00.000Z',
+      data_transacao: '2020-08-22T03:00:00.000Z',
+      descricao: 'Desc',
+      envolvido: 'Inter',
+      id: 219754,
+      observacao: null,
+      parcelamento_id: null,
+      status: true,
+      tipo: 'REC',
+      transferencia_id: null,
+      usuario_id: 11053,
+      valor: '123.00',
     }).as('saveMovimentacao');
 
-    cy.route({
-      method: 'GET',
-      url: '/extrato/**',
-      response: 'fixture:movimentacaoSalva',
-    }).as('extrato');
+    cy.intercept('GET', '/extrato/**', { fixture: 'movimentacaoSalva' }).as(
+      'extrato',
+    );
 
     cy.get(loc.MENU.MOVIMENTACAO).click();
 
@@ -149,61 +133,59 @@ describe('Should test at a functional level', () => {
   });
 
   it('Should get updated balance', () => {
-    cy.route({
-      method: 'GET',
-      url: '/transacoes/**',
-      response: {
-        conta: 'Conta para saldo',
-        id: 219571,
-        descricao: 'Movimentacao 1, calculo saldo',
-        envolvido: 'CCC',
-        observacao: null,
-        tipo: 'REC',
-        data_transacao: '2020-08-22T03:00:00.000Z',
-        data_pagamento: '2020-08-22T03:00:00.000Z',
-        valor: '3500.00',
-        status: false,
-        conta_id: 245138,
-        usuario_id: 11053,
-        transferencia_id: null,
-        parcelamento_id: null,
-      },
+    cy.intercept('GET', '/transacoes/**', {
+      conta: 'Conta para saldo',
+      id: 219571,
+      descricao: 'Movimentacao 1, calculo saldo',
+      envolvido: 'CCC',
+      observacao: null,
+      tipo: 'REC',
+      data_transacao: '2020-08-22T03:00:00.000Z',
+      data_pagamento: '2020-08-22T03:00:00.000Z',
+      valor: '3500.00',
+      status: false,
+      conta_id: 245138,
+      usuario_id: 11053,
+      transferencia_id: null,
+      parcelamento_id: null,
     });
 
-    cy.route({
-      method: 'PUT',
-      url: '/transacoes/**',
-      response: {
-        conta: 'Conta para saldo',
-        id: 219571,
-        descricao: 'Movimentacao 1, calculo saldo',
-        envolvido: 'CCC',
-        observacao: null,
-        tipo: 'REC',
-        data_transacao: '2020-08-22T03:00:00.000Z',
-        data_pagamento: '2020-08-22T03:00:00.000Z',
-        valor: '3500.00',
-        status: false,
-        conta_id: 245138,
-        usuario_id: 11053,
-        transferencia_id: null,
-        parcelamento_id: null,
-      },
+    cy.intercept('PUT', '/transacoes/**', {
+      conta: 'Conta para saldo',
+      id: 219571,
+      descricao: 'Movimentacao 1, calculo saldo',
+      envolvido: 'CCC',
+      observacao: null,
+      tipo: 'REC',
+      data_transacao: '2020-08-22T03:00:00.000Z',
+      data_pagamento: '2020-08-22T03:00:00.000Z',
+      valor: '3500.00',
+      status: false,
+      conta_id: 245138,
+      usuario_id: 11053,
+      transferencia_id: null,
+      parcelamento_id: null,
     });
 
-    cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Carteira')).should('contain', '100,00');
+    cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Carteira')).should(
+      'contain',
+      '100,00',
+    );
 
     cy.get(loc.MENU.EXTRATO).click();
-    cy.xpath(loc.EXTRATO.FN_XP_BTN_EDITAR('Movimentacao 1, calculo saldo')).click();
-    cy.get(loc.MOVIMENTACAO.DESCRICAO).should('have.value', 'Movimentacao 1, calculo saldo');
+    cy.xpath(
+      loc.EXTRATO.FN_XP_BTN_EDITAR('Movimentacao 1, calculo saldo'),
+    ).click();
+    cy.get(loc.MOVIMENTACAO.DESCRICAO).should(
+      'have.value',
+      'Movimentacao 1, calculo saldo',
+    );
     cy.get(loc.MOVIMENTACAO.STATUS).click();
     cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click();
     cy.get(loc.MESSAGE).should('contain', 'alterada com sucesso');
 
-    cy.route({
-      method: 'GET',
-      url: '/saldo',
-      response: [{
+    cy.intercept('GET', '/saldo', [
+      {
         conta_id: 999,
         conta: 'Carteira',
         saldo: '4034.00',
@@ -213,95 +195,163 @@ describe('Should test at a functional level', () => {
         conta: 'Banco',
         saldo: '10000000,00',
       },
-      ],
-    }).as('saldoFinal');
+    ]).as('saldoFinal');
 
     cy.get(loc.MENU.HOME).click();
-    cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Carteira')).should('contain', '4.034,00');
+    cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Carteira')).should(
+      'contain',
+      '4.034,00',
+    );
   });
 
   it('Should remove a transaction', () => {
-    cy.route({
-      method: 'DELETE',
-      url: '/transacoes/**',
-      response: {},
-      status: 204,
+    cy.intercept('DELETE', '/transacoes/**', {
+      body: {},
+      statusCode: 204,
     }).as('del');
 
     cy.get(loc.MENU.EXTRATO).click();
-    cy.xpath(loc.EXTRATO.FN_XP_BTN_EXCLUIR('Movimentacao para exclusao')).click();
+    cy.xpath(
+      loc.EXTRATO.FN_XP_BTN_EXCLUIR('Movimentacao para exclusao'),
+    ).click();
     cy.get(loc.MESSAGE).should('contain', 'removida com sucesso');
   });
 
   it('Should validate data sent to create an account', () => {
-    const reqStub = cy.stub();
-    cy.route({
-      method: 'POST',
-      url: '/contas',
-      response: [
-        {
-          id: 3, nome: 'Conta de teste', visivel: true, usuario_id: 1000,
-        },
-      ],
+    cy.intercept(
+      'POST',
+      '/contas',
+      {
+        id: 3,
+        nome: 'Conta de teste',
+        visivel: true,
+        usuario_id: 1000,
+      },
+      // After cy.route() got removed, this example became obsolete
       // onRequest: (req) => {
       //   console.log(req);
       //   expect(req.request.body.nome).to.be.empty;
       //   expect(req.request.headers).to.have.property('Authorization');
       // },
-      onRequest: reqStub,
-    }).as('saveConta');
+      // onRequest: reqStub,
+    ).as('saveConta');
 
     cy.acessarMenuConta();
 
-    cy.route({
-      method: 'GET',
-      url: '/contas',
-      response: [
-        {
-          id: 1, nome: 'Carteira', visivel: true, usuario_id: 1000,
-        },
-        {
-          id: 2, nome: 'Banco', visivel: true, usuario_id: 1000,
-        },
-        {
-          id: 3, nome: 'Conta de teste', visivel: true, usuario_id: 1000,
-        },
-      ],
-    }).as('contasSave');
+    cy.intercept('GET', '/contas', [
+      {
+        id: 1,
+        nome: 'Carteira',
+        visivel: true,
+        usuario_id: 1000,
+      },
+      {
+        id: 2,
+        nome: 'Banco',
+        visivel: true,
+        usuario_id: 1000,
+      },
+      {
+        id: 3,
+        nome: 'Conta de teste',
+        visivel: true,
+        usuario_id: 1000,
+      },
+    ]).as('contasSave');
 
     cy.inserirConta('{CONTROL}');
     // cy.wait('@saveConta').its('request.body.nome').should('not.be.empty');
-    cy.wait('@saveConta').then(() => {
-      console.log(reqStub.args[0][0]);
-      expect(reqStub.args[0][0].request.body.nome).to.be.empty;
-      expect(reqStub.args[0][0].request.headers).to.have.property('Authorization');
+    cy.wait('@saveConta').then((reqStub) => {
+      console.log(reqStub.request);
+      expect(reqStub.request.body.nome).to.be.empty;
+      expect(reqStub.request.headers).to.have.property('authorization');
     });
     cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso');
   });
 
   it('Should test colors', () => {
-    cy.route({
-      method: 'GET',
-      url: '/extrato/**',
-      response: [
-        {
-          conta: 'Conta para movimentacoes', id: 219569, descricao: 'Receita paga', envolvido: 'AAA', observacao: null, tipo: 'REC', data_transacao: '2020-08-22T03:00:00.000Z', data_pagamento: '2020-08-22T03:00:00.000Z', valor: '-1500.00', status: true, conta_id: 245136, usuario_id: 11053, transferencia_id: null, parcelamento_id: null,
-        },
-        {
-          conta: 'Conta com movimentacao', id: 219570, descricao: 'Receita pendente', envolvido: 'BBB', observacao: null, tipo: 'REC', data_transacao: '2020-08-22T03:00:00.000Z', data_pagamento: '2020-08-22T03:00:00.000Z', valor: '-1500.00', status: false, conta_id: 245137, usuario_id: 11053, transferencia_id: null, parcelamento_id: null,
-        },
-        {
-          conta: 'Conta para saldo', id: 219571, descricao: 'Despesa paga', envolvido: 'CCC', observacao: null, tipo: 'DESP', data_transacao: '2020-08-22T03:00:00.000Z', data_pagamento: '2020-08-22T03:00:00.000Z', valor: '3500.00', status: true, conta_id: 245138, usuario_id: 11053, transferencia_id: null, parcelamento_id: null,
-        },
-        {
-          conta: 'Conta para saldo', id: 219572, descricao: 'Despesa pendente', envolvido: 'DDD', observacao: null, tipo: 'DESP', data_transacao: '2020-08-22T03:00:00.000Z', data_pagamento: '2020-08-22T03:00:00.000Z', valor: '-1000.00', status: false, conta_id: 245138, usuario_id: 11053, transferencia_id: null, parcelamento_id: null,
-        },
-      ],
-    }).as('extrato');
+    cy.intercept('GET', '/extrato/**', [
+      {
+        conta: 'Conta para movimentacoes',
+        id: 219569,
+        descricao: 'Receita paga',
+        envolvido: 'AAA',
+        observacao: null,
+        tipo: 'REC',
+        data_transacao: '2020-08-22T03:00:00.000Z',
+        data_pagamento: '2020-08-22T03:00:00.000Z',
+        valor: '-1500.00',
+        status: true,
+        conta_id: 245136,
+        usuario_id: 11053,
+        transferencia_id: null,
+        parcelamento_id: null,
+      },
+      {
+        conta: 'Conta com movimentacao',
+        id: 219570,
+        descricao: 'Receita pendente',
+        envolvido: 'BBB',
+        observacao: null,
+        tipo: 'REC',
+        data_transacao: '2020-08-22T03:00:00.000Z',
+        data_pagamento: '2020-08-22T03:00:00.000Z',
+        valor: '-1500.00',
+        status: false,
+        conta_id: 245137,
+        usuario_id: 11053,
+        transferencia_id: null,
+        parcelamento_id: null,
+      },
+      {
+        conta: 'Conta para saldo',
+        id: 219571,
+        descricao: 'Despesa paga',
+        envolvido: 'CCC',
+        observacao: null,
+        tipo: 'DESP',
+        data_transacao: '2020-08-22T03:00:00.000Z',
+        data_pagamento: '2020-08-22T03:00:00.000Z',
+        valor: '3500.00',
+        status: true,
+        conta_id: 245138,
+        usuario_id: 11053,
+        transferencia_id: null,
+        parcelamento_id: null,
+      },
+      {
+        conta: 'Conta para saldo',
+        id: 219572,
+        descricao: 'Despesa pendente',
+        envolvido: 'DDD',
+        observacao: null,
+        tipo: 'DESP',
+        data_transacao: '2020-08-22T03:00:00.000Z',
+        data_pagamento: '2020-08-22T03:00:00.000Z',
+        valor: '-1000.00',
+        status: false,
+        conta_id: 245138,
+        usuario_id: 11053,
+        transferencia_id: null,
+        parcelamento_id: null,
+      },
+    ]).as('extrato');
     cy.get(loc.MENU.EXTRATO).click();
-    cy.xpath(loc.EXTRATO.FN_XP_LINHA('Receita paga')).should('have.class', 'receitaPaga');
-    cy.xpath(loc.EXTRATO.FN_XP_LINHA('Receita pendente')).should('have.class', 'receitaPendente');
-    cy.xpath(loc.EXTRATO.FN_XP_LINHA('Despesa paga')).should('have.class', 'despesaPaga');
-    cy.xpath(loc.EXTRATO.FN_XP_LINHA('Despesa pendente')).should('have.class', 'despesaPendente');
+    cy.xpath(loc.EXTRATO.FN_XP_LINHA('Receita paga')).should(
+      'have.class',
+      'receitaPaga',
+    );
+    cy.xpath(loc.EXTRATO.FN_XP_LINHA('Receita pendente')).should(
+      'have.class',
+      'receitaPendente',
+    );
+    cy.xpath(loc.EXTRATO.FN_XP_LINHA('Despesa paga')).should(
+      'have.class',
+      'despesaPaga',
+    );
+    cy.xpath(loc.EXTRATO.FN_XP_LINHA('Despesa pendente')).should(
+      'have.class',
+      'despesaPendente',
+    );
   });
 });
